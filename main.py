4a629e5439522937dfbfbd949a857081729a0791
Derick Nguyen <100515167+Dericknlaser@users.noreply.github.com>
2023-03-23 06:10:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from website import createApp
-app = createApp()
+from website import create_app
 
-# if we run this file (main.py), then we run the app
-# if you import main.py from another file, then web server would automatically run
-# we only want web server to run if you execute main.py
+app = create_app()
+
 if __name__ == '__main__':
-    # app.run --> starts up a web server, debug=True means web server re runs anytime a change is made
     app.run(debug=True)
--- website/__init__.py
+++ website/__init__.py
@@ -1,19 +1,41 @@
-# whatever is inside this will run automatically when we import this folder
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
-# create app, initialize secret key, return the app
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
-def createApp():
+def create_app():
     app = Flask(__name__)
-    # secret key for the app, doesnt matter what this is
-    app.config['SECRET_KEY'] = 'abc123456789'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
-    # we have blueprints containing URL, here is where they are
     from .views import views
     from .auth import auth
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
