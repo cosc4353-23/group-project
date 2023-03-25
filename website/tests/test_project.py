4f2f7a06e87b8568ef3ae7551c37da344600df30
eyiece-krheme <msasummers@yahoo.com>
2023-03-24 22:48:21 -0500

    test new user

--- website/tests/test_project.py
+++ website/tests/test_project.py
@@ -1,4 +1,12 @@
-def test_home(client):
-    response = client.get("/login?next=%2F")
-    assert b"<title>Home</title>" in response.data  
+from website.models import User
 
+def test_home(client):
+    response = client.get("/login")
+    assert b"<title>Login</title>" in response.data  
+
+def test_registration(client, app):
+    response = client.post("/sign-up", data={"email": "test@test.com", "password1": "testpassword", "password2": "testpassword"})
+    print(response.data)
+    with app.app_context():
+        assert User.query.count() == 1
+        assert User.query.first().email == "test@test.com"
