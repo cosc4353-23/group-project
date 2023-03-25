a2e46b053ccfdc934b39f306e5e32c726d9bf440
eyiece-krheme <msasummers@yahoo.com>
2023-03-24 23:09:03 -0500

    test profile

--- website/tests/test_project.py
+++ website/tests/test_project.py
@@ -6,7 +6,19 @@ def test_home(client):
 
 def test_registration(client, app):
     response = client.post("/sign-up", data={"email": "test@test.com", "password1": "testpassword", "password2": "testpassword"})
-    print(response.data)
     with app.app_context():
         assert User.query.count() == 1
         assert User.query.first().email == "test@test.com"
+
+def test_profile(client, app):
+    client.post("/sign-up", data={"email": "test@test.com", "password1": "testpassword", "password2": "testpassword"})
+    client.post("/login", data={"email": "test@test.com", "password": "testpassword"})
+
+    response = client.post("/profile", data={"fullName": "John Smith", "address1": "123 Main", "address2": "appt 1", "city": "houston", "state": "Tx", "zipcode": "77001"})
+    with app.app_context():
+        assert User.query.first().fullName == "John Smith"
+        assert User.query.first().address1 == "123 Main"
+        assert User.query.first().address2 == "appt 1"
+        assert User.query.first().city == "houston"
+        assert User.query.first().state == "Tx"
+        assert User.query.first().zipcode == "77001"
