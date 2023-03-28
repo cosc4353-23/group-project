d7a875a966ca7853aeb11d2669f3840fd784037d
Michael Summers <msasummers@yahoo.com>
2023-03-28 11:46:31 -0500

    remove debug comments

--- website/__init__.py
+++ website/__init__.py
@@ -6,23 +6,6 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-# def create_app(database_uri="sqlite:///db.sqlite3"):
-#     app = Flask(__name__)
-#     app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
-#     app.config["SECRET_KEY"] = "FesC9cBSuxakv9yN0vBY"
-
-#     db.init_app(app)
-#     login_manager.init_app(app)
-
-#     @login_manager.user_loader
-#     def load_user(user_id):
-#         return User.query.get(user_id)
-
-#     app.register_blueprint(main)
-#     return app
-
-
-
 def create_app(database_uri="sqlite:///db.sqlite3"):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
